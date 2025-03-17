c2b031c0ac00b202de6b318253d7bb6acb65518b
Obaid Ahmed <obaida7@gmail.com>
2025-03-16 17:16:23 -0700

    retry

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name               = cluster1
+  name               = var.cluster_name
   location           = var.region
   remove_default_node_pool = true
   initial_node_count = 1
